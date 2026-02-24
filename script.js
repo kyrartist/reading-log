1c958d1167b4c360a6a701adbc6c36039b7459ea
Kyrartist <kyrartist@gmail.com>
2026-02-23 18:02:15 -0600

    Update rating system to include half stars

--- script.js
+++ script.js
@@ -11,26 +11,45 @@ addBookForm.addEventListener("submit", (event) => {
 
   const title = dataObject.title;
   const author = dataObject.author;
-  let rating = dataObject.rating;
+  let rating = parseFloat(dataObject.rating);
   console.log(typeof rating);
 
   const fullStar = '<i class="bi bi-star-fill">';
+  const halfStar = '<i class="bi bi-star-half"></i>';
   const emptyStar = '<i class="bi bi-star"></i>';
 
-  switch (rating) {
-    case "1":
+  switch (true) {
+    case rating < 0.5:
+      rating = `${emptyStar}${emptyStar}${emptyStar}${emptyStar}${emptyStar}`;
+      break;
+    case rating < 1:
+      rating = `${halfStar}${emptyStar}${emptyStar}${emptyStar}${emptyStar}`;
+      break;
+    case rating < 1.5:
       rating = `${fullStar}${emptyStar}${emptyStar}${emptyStar}${emptyStar}`;
       break;
-    case "2":
+    case rating < 2:
+      rating = `${fullStar}${halfStar}${emptyStar}${emptyStar}${emptyStar}`;
+      break;
+    case rating < 2.5:
       rating = `${fullStar}${fullStar}${emptyStar}${emptyStar}${emptyStar}`;
       break;
-    case "3":
+    case rating < 3:
+      rating = `${fullStar}${fullStar}${halfStar}${emptyStar}${emptyStar}`;
+      break;
+    case rating < 3.5:
       rating = `${fullStar}${fullStar}${fullStar}${emptyStar}${emptyStar}`;
       break;
-    case "4":
+    case rating < 4:
+      rating = `${fullStar}${fullStar}${fullStar}${halfStar}${emptyStar}`;
+      break;
+    case rating < 4.5:
       rating = `${fullStar}${fullStar}${fullStar}${fullStar}${emptyStar}`;
       break;
-    case "5":
+    case rating < 5:
+      rating = `${fullStar}${fullStar}${fullStar}${fullStar}${halfStar}`;
+      break;
+    case rating === 5:
       rating = `${fullStar}${fullStar}${fullStar}${fullStar}${fullStar}`;
       break;
   }
